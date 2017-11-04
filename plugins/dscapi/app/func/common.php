c9174df1809b96454155ea931e20d3e1043bbf6b
Fany <wangpengcheng@192.168.2.138>
2017-11-04 12:07:12 +0800

    sc

--- plugins/dscapi/app/func/common.php
+++ plugins/dscapi/app/func/common.php
@@ -1,186 +1,170 @@
 <?php
-//zend by QQ:2172298892  瑾梦网络  禁止倒卖 一经发现停止任何服务
+
 namespace app\func;
 
-class common
-{
-	static private $format = 'json';
-	static private $page_size = 10;
-	static private $page = 1;
-	static private $charset = 'utf-8';
-	static private $result;
-	static private $msg;
-	static private $error;
-	static private $allowOutputType = array('xml' => 'application/xml', 'json' => 'application/json', 'html' => 'text/html');
+class common {
 
-	public function __construct($data = array())
-	{
-		self::common($data);
-	}
+    static private $format = 'json';
+    static private $page_size = 10;
+    static private $page = 1;
+    static private $charset = 'utf-8';
+    static private $result;
+    static private $msg;
+    static private $error;
+    static private $allowOutputType = array('xml' => 'application/xml', 'json' => 'application/json', 'html' => 'text/html');
 
-	static public function common($data = array())
-	{
-		self::$format = (isset($data['format']) ? $data['format'] : 'josn');
-		self::$page_size = (isset($data['page_size']) ? $data['page_size'] : 10);
-		self::$page = (isset($data['page']) ? $data['page'] : 1);
-		self::$msg = (isset($data['msg']) ? $data['msg'] : '');
-		self::$result = (isset($data['result']) ? $data['result'] : 'success');
-		self::$error = (isset($data['error']) ? $data['error'] : 0);
-	}
+    public function __construct($data = array()) {
+        self::common($data);
+    }
 
-	static public function data_back($info = array(), $arr_type = 0)
-	{
-		if ($arr_type == 1) {
-			$list = self::page_array(self::$page_size, self::$page, $info);
-			$info = $list;
-		}
+    static public function common($data = array()) {
+        self::$format = (isset($data['format']) ? $data['format'] : 'josn');
+        self::$page_size = (isset($data['page_size']) ? $data['page_size'] : 10);
+        self::$page = (isset($data['page']) ? $data['page'] : 1);
+        self::$msg = (isset($data['msg']) ? $data['msg'] : '');
+        self::$result = (isset($data['result']) ? $data['result'] : 'success');
+        self::$error = (isset($data['error']) ? $data['error'] : 0);
+    }
 
-		$data_arr = array('result' => self::$result, 'error' => self::$error, 'msg' => self::$msg);
+    static public function data_back($info = array(), $arr_type = 0) {
+        if ($arr_type == 1) {
+            $list = self::page_array(self::$page_size, self::$page, $info);
+            $info = $list;
+        }
 
-		if ($info) {
-			$data_arr['info'] = $info;
-		}
+        $data_arr = array('result' => self::$result, 'error' => self::$error, 'msg' => self::$msg);
 
-		$data_arr = self::to_utf8_iconv($data_arr);
+        if ($info) {
+            $data_arr['info'] = $info;
+        }
 
-		if (self::$format == 'xml') {
-			if (isset(self::$allowOutputType[self::$format])) {
-				header('Content-Type: ' . self::$allowOutputType[self::$format] . '; charset=' . self::$charset);
-			}
+        $data_arr = self::to_utf8_iconv($data_arr);
 
-			return self::xml_encode($data_arr);
-		}
-		else {
-			if (isset(self::$allowOutputType[self::$format])) {
-				header('Content-Type: ' . self::$allowOutputType[self::$format] . '; charset=' . self::$charset);
-			}
+        if (self::$format == 'xml') {
+            if (isset(self::$allowOutputType[self::$format])) {
+                header('Content-Type: ' . self::$allowOutputType[self::$format] . '; charset=' . self::$charset);
+            }
 
-			return json_encode($data_arr);
-		}
-	}
+            return self::xml_encode($data_arr);
+        } else {
+            if (isset(self::$allowOutputType[self::$format])) {
+                header('Content-Type: ' . self::$allowOutputType[self::$format] . '; charset=' . self::$charset);
+            }
 
-	static public function xml_encode($data, $root = 'dsc', $item = 'item', $attr = '', $id = 'id', $encoding = 'utf-8')
-	{
-		if (is_array($attr)) {
-			$_attr = array();
+            return json_encode($data_arr);
+        }
+    }
 
-			foreach ($attr as $key => $value) {
-				$_attr[] = $key . '="' . $value . '"';
-			}
+    static public function xml_encode($data, $root = 'dsc', $item = 'item', $attr = '', $id = 'id', $encoding = 'utf-8') {
+        if (is_array($attr)) {
+            $_attr = array();
 
-			$attr = implode(' ', $_attr);
-		}
+            foreach ($attr as $key => $value) {
+                $_attr[] = $key . '="' . $value . '"';
+            }
 
-		$attr = trim($attr);
-		$attr = (empty($attr) ? '' : ' ' . $attr);
-		$xml = '<?xml version="1.0" encoding="' . $encoding . '"?>';
-		$xml .= '<' . $root . $attr . '>';
-		$xml .= self::data_to_xml($data, $item, $id);
-		$xml .= '</' . $root . '>';
-		return $xml;
-	}
+            $attr = implode(' ', $_attr);
+        }
 
-	static public function data_to_xml($data, $item = 'item', $id = 'id')
-	{
-		$xml = $attr = '';
+        $attr = trim($attr);
+        $attr = (empty($attr) ? '' : ' ' . $attr);
+        $xml = '<?xml version="1.0" encoding="' . $encoding . '"?>';
+        $xml .= '<' . $root . $attr . '>';
+        $xml .= self::data_to_xml($data, $item, $id);
+        $xml .= '</' . $root . '>';
+        return $xml;
+    }
 
-		foreach ($data as $key => $val) {
-			if (is_numeric($key)) {
-				$id && ($attr = ' ' . $id . '="' . $key . '"');
-				$key = $item;
-			}
+    static public function data_to_xml($data, $item = 'item', $id = 'id') {
+        $xml = $attr = '';
 
-			$xml .= '<' . $key . $attr . '>';
-			$xml .= (is_array($val) || is_object($val) ? self::data_to_xml($val, $item, $id) : $val);
-			$xml .= '</' . $key . '>';
-		}
+        foreach ($data as $key => $val) {
+            if (is_numeric($key)) {
+                $id && ($attr = ' ' . $id . '="' . $key . '"');
+                $key = $item;
+            }
 
-		return $xml;
-	}
+            $xml .= '<' . $key . $attr . '>';
+            $xml .= (is_array($val) || is_object($val) ? self::data_to_xml($val, $item, $id) : $val);
+            $xml .= '</' . $key . '>';
+        }
 
-	static public function to_utf8_iconv($str)
-	{
-		if (EC_CHARSET != 'utf-8') {
-			if (is_string($str)) {
-				return ecs_iconv(EC_CHARSET, 'utf-8', $str);
-			}
-			else if (is_array($str)) {
-				foreach ($str as $key => $value) {
-					$str[$key] = to_utf8_iconv($value);
-				}
+        return $xml;
+    }
 
-				return $str;
-			}
-			else if (is_object($str)) {
-				foreach ($str as $key => $value) {
-					$str->$key = to_utf8_iconv($value);
-				}
+    static public function to_utf8_iconv($str) {
+        if (EC_CHARSET != 'utf-8') {
+            if (is_string($str)) {
+                return ecs_iconv(EC_CHARSET, 'utf-8', $str);
+            } else if (is_array($str)) {
+                foreach ($str as $key => $value) {
+                    $str[$key] = to_utf8_iconv($value);
+                }
 
-				return $str;
-			}
-			else {
-				return $str;
-			}
-		}
+                return $str;
+            } else if (is_object($str)) {
+                foreach ($str as $key => $value) {
+                    $str->$key = to_utf8_iconv($value);
+                }
 
-		return $str;
-	}
+                return $str;
+            } else {
+                return $str;
+            }
+        }
 
-	static public function page_array($page_size = 1, $page = 1, $array = array(), $order = 0)
-	{
-		$arr = array();
-		$pagedata = array();
+        return $str;
+    }
 
-		if ($array) {
-			global $countpage;
-			$start = ($page - 1) * $page_size;
+    static public function page_array($page_size = 1, $page = 1, $array = array(), $order = 0) {
+        $arr = array();
+        $pagedata = array();
 
-			if ($order == 1) {
-				$array = array_reverse($array);
-			}
+        if ($array) {
+            global $countpage;
+            $start = ($page - 1) * $page_size;
 
-			if (isset($array['record_count'])) {
-				$totals = $array['record_count'];
-				$countpage = ceil($totals / $page_size);
-				$pagedata = $array['list'];
-			}
-			else {
-				$totals = count($array);
-				$countpage = ceil($totals / $page_size);
-				$pagedata = array_slice($array, $start, $page_size);
-			}
+            if ($order == 1) {
+                $array = array_reverse($array);
+            }
 
-			$filter = array('page' => $page, 'page_size' => $page_size, 'record_count' => $totals, 'page_count' => $countpage);
-			$arr = array('list' => $pagedata, 'filter' => $filter, 'page_count' => $countpage, 'record_count' => $totals);
-		}
+            if (isset($array['record_count'])) {
+                $totals = $array['record_count'];
+                $countpage = ceil($totals / $page_size);
+                $pagedata = $array['list'];
+            } else {
+                $totals = count($array);
+                $countpage = ceil($totals / $page_size);
+                $pagedata = array_slice($array, $start, $page_size);
+            }
 
-		return $arr;
-	}
+            $filter = array('page' => $page, 'page_size' => $page_size, 'record_count' => $totals, 'page_count' => $countpage);
+            $arr = array('list' => $pagedata, 'filter' => $filter, 'page_count' => $countpage, 'record_count' => $totals);
+        }
 
-	static public function get_reference_only($table, $where = 1, $select = '', $type = 0)
-	{
-		if (!empty($select) && is_array($select)) {
-			$select = implode(',', $select);
-		}
-		else {
-			$select = '*';
-		}
+        return $arr;
+    }
 
-		$sql = 'SELECT ' . $select . ' FROM ' . $GLOBALS['ecs']->table($table) . ' WHERE ' . $where;
+    static public function get_reference_only($table, $where = 1, $select = '', $type = 0) {
+        if (!empty($select) && is_array($select)) {
+            $select = implode(',', $select);
+        } else {
+            $select = '*';
+        }
 
-		if ($type == 1) {
-			return $GLOBALS['db']->getRow($sql);
-		}
-		else {
-			return $GLOBALS['db']->getOne($sql);
-		}
-	}
+        $sql = 'SELECT ' . $select . ' FROM ' . $GLOBALS['ecs']->table($table) . ' WHERE ' . $where;
 
-	public function __callStatic($method, $arguments)
-	{
-		return call_user_func_array(array(self, $method), $arguments);
-	}
+        if ($type == 1) {
+            return $GLOBALS['db']->getRow($sql);
+        } else {
+            return $GLOBALS['db']->getOne($sql);
+        }
+    }
+
+    public function __callStatic($method, $arguments) {
+        return call_user_func_array(array(self, $method), $arguments);
+    }
+
 }
-
 
 ?>
