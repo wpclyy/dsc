c6802d4733a64b41d72e2e158b4df403b3464fee
Fany <wangpengcheng@192.168.2.138>
2017-11-09 15:46:00 +0800

    sc

--- plugins/dscapi/view/attribute.php
+++ plugins/dscapi/view/attribute.php
@@ -1,5 +1,5 @@
 <?php
-//zend by QQ:2172298892  瑾梦网络  禁止倒卖 一经发现停止任何服务
+
 $seller_type = (isset($_REQUEST['seller_type']) ? $base->get_intval($_REQUEST['seller_type']) : -1);
 $seller_id = (isset($_REQUEST['seller_id']) ? $base->get_intval($_REQUEST['seller_id']) : -1);
 $attr_id = (isset($_REQUEST['attr_id']) ? $base->get_intval($_REQUEST['attr_id']) : -1);
@@ -10,69 +10,68 @@ $val = array('seller_type' => $seller_type, 'seller_id' => $seller_id, 'attr_id'
 $attribute = new \app\controller\attribute($val);
 
 switch ($method) {
-case 'dsc.goodstype.list.get':
-	$table = array('goodstype' => 'goods_type');
-	$result = $attribute->get_goodstype_list($table);
-	exit($result);
-	break;
+    case 'dsc.goodstype.list.get':
+        $table = array('goodstype' => 'goods_type');
+        $result = $attribute->get_goodstype_list($table);
+        exit($result);
+        break;
 
-case 'dsc.goodstype.info.get':
-	$table = array('goodstype' => 'goods_type');
-	$result = $attribute->get_goodstype_info($table);
-	exit($result);
-	break;
+    case 'dsc.goodstype.info.get':
+        $table = array('goodstype' => 'goods_type');
+        $result = $attribute->get_goodstype_info($table);
+        exit($result);
+        break;
 
-case 'dsc.goodstype.insert.post':
-	$table = array('goodstype' => 'goods_type');
-	$result = $attribute->get_goodstype_insert($table);
-	exit($result);
-	break;
+    case 'dsc.goodstype.insert.post':
+        $table = array('goodstype' => 'goods_type');
+        $result = $attribute->get_goodstype_insert($table);
+        exit($result);
+        break;
 
-case 'dsc.goodstype.update.post':
-	$table = array('goodstype' => 'goods_type');
-	$result = $attribute->get_goodstype_update($table);
-	exit($result);
-	break;
+    case 'dsc.goodstype.update.post':
+        $table = array('goodstype' => 'goods_type');
+        $result = $attribute->get_goodstype_update($table);
+        exit($result);
+        break;
 
-case 'dsc.goodstype.del.post':
-	$table = array('goodstype' => 'goods_type');
-	$result = $attribute->get_goodstype_delete($table);
-	exit($result);
-	break;
+    case 'dsc.goodstype.del.post':
+        $table = array('goodstype' => 'goods_type');
+        $result = $attribute->get_goodstype_delete($table);
+        exit($result);
+        break;
 
-case 'dsc.attribute.list.get':
-	$table = array('attribute' => 'attribute');
-	$result = $attribute->get_attribute_list($table);
-	exit($result);
-	break;
+    case 'dsc.attribute.list.get':
+        $table = array('attribute' => 'attribute');
+        $result = $attribute->get_attribute_list($table);
+        exit($result);
+        break;
 
-case 'dsc.attribute.info.get':
-	$table = array('attribute' => 'attribute');
-	$result = $attribute->get_attribute_info($table);
-	exit($result);
-	break;
+    case 'dsc.attribute.info.get':
+        $table = array('attribute' => 'attribute');
+        $result = $attribute->get_attribute_info($table);
+        exit($result);
+        break;
 
-case 'dsc.attribute.insert.post':
-	$table = array('attribute' => 'attribute');
-	$result = $attribute->get_attribute_insert($table);
-	exit($result);
-	break;
+    case 'dsc.attribute.insert.post':
+        $table = array('attribute' => 'attribute');
+        $result = $attribute->get_attribute_insert($table);
+        exit($result);
+        break;
 
-case 'dsc.attribute.update.post':
-	$table = array('attribute' => 'attribute');
-	$result = $attribute->get_attribute_update($table);
-	exit($result);
-	break;
+    case 'dsc.attribute.update.post':
+        $table = array('attribute' => 'attribute');
+        $result = $attribute->get_attribute_update($table);
+        exit($result);
+        break;
 
-case 'dsc.attribute.del.post':
-	$table = array('attribute' => 'attribute');
-	$result = $attribute->get_attribute_delete($table);
-	exit($result);
-	break;
+    case 'dsc.attribute.del.post':
+        $table = array('attribute' => 'attribute');
+        $result = $attribute->get_attribute_delete($table);
+        exit($result);
+        break;
 
-default:
-	echo '非法接口连接';
-	break;
+    default:
+        echo '非法接口连接';
+        break;
 }
-
 ?>
